772eb9c8edb473ce8ae43a2b8c4b12c385e4b9a2
Mark DeLillo <github.med@delillo.org>
2017-03-08 22:47:37 -0500

    Use slack client in main

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
 	"strings"
 
 	"flag"
-	"golang.org/x/net/websocket"
-	"net/url"
+	"github.com/mdelillo/claimer/slack"
 )
 
 func main() {
@@ -17,72 +13,22 @@ func main() {
 	flag.String("deployKey", "", "Deploy key for Github")
 	flag.Parse()
 
-	resp, err := http.Get("https://slack.com/api/rtm.start?token=" + *apiToken)
-	if err != nil {
-		panic(err)
-	}
-	if resp.StatusCode != 200 {
-		panic("Bad response from slack: " + resp.Status)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	resp.Body.Close()
-
-	var rtmStart struct {
-		Ok    bool
-		Error string
-		Url   string
-		Self  struct {
-			Id string
-		}
-	}
-
-	if err := json.Unmarshal(body, &rtmStart); err != nil {
-		panic(err)
-	}
-	if !rtmStart.Ok {
-		panic("RTM start failed: " + rtmStart.Error)
-	}
-
-	botId := rtmStart.Self.Id
-
-	ws, err := websocket.Dial(rtmStart.Url, "", "https://api.slack.com/")
+	client := slack.NewClient("https://slack.com", *apiToken)
+	messages, err := client.Listen()
 	if err != nil {
 		panic(err)
 	}
 
-	for {
-		var message struct {
-			Type    string
-			Text    string
-			Channel string
-		}
-		if err := websocket.JSON.Receive(ws, &message); err != nil {
-			panic(err)
-		}
-		if message.Type == "message" && strings.HasPrefix(message.Text, "<@"+botId+">") {
-			splitMessage := strings.Fields(message.Text)
-			switch splitMessage[1] {
-			case "claim":
-				_, err := http.PostForm(
-					"https://slack.com/api/chat.postMessage",
-					url.Values{
-						"token":   {*apiToken},
-						"channel": {message.Channel},
-						"text":    {"Claimed pool-1"},
-						"as_user": {"true"},
-					},
-				)
-				if err != nil {
-					panic(err)
-				}
-				// TODO check response
-			default:
-				panic("Command " + splitMessage[1] + " not implemented")
+	for message := range messages {
+		splitMessage := strings.Fields(message.Text)
+		switch splitMessage[1] {
+		case "claim":
+			if err := client.PostMessage(message.Channel, "Claimed pool-1"); err != nil {
+				panic(err)
 			}
+		// TODO check response
+		default:
+			panic("Command " + splitMessage[1] + " not implemented")
 		}
 	}
 }
