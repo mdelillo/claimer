2ed27e98205dfd4735808623dda9d27d3176e0c4
Mark DeLillo <github.med@delillo.org>
2017-03-08 22:47:37 -0500

    Add slack.Listen() method to receive messages for user

--- slack/client.go
+++ slack/client.go
@@ -3,9 +3,11 @@ package slack
 import (
 	"encoding/json"
 	"fmt"
+	"golang.org/x/net/websocket"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 type client struct {
@@ -18,6 +20,73 @@ func NewClient(url, apiToken string) *client {
 		apiToken: apiToken,
 		url:      url,
 	}
+}
+
+type Message struct {
+	Type    string
+	Text    string
+	Channel string
+}
+
+func (c *client) Listen() (<-chan *Message, error) {
+	resp, err := http.Get(c.url + "/api/rtm.start?token=" + c.apiToken)
+	if err != nil {
+		panic(err)
+	}
+	if resp.StatusCode != 200 {
+		panic("error from slack:: " + resp.Status)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+
+	var rtmStart struct {
+		Ok    bool
+		Error string
+		Url   string
+		Self  struct {
+			Id string
+		}
+	}
+
+	if err := json.Unmarshal(body, &rtmStart); err != nil {
+		panic(err)
+	}
+	if !rtmStart.Ok {
+		panic("error from slack: " + rtmStart.Error)
+	}
+
+	botId := rtmStart.Self.Id
+
+	ws, err := websocket.Dial(rtmStart.Url, "", "https://api.slack.com/")
+	if err != nil {
+		panic(err)
+	}
+
+	messageChan := make(chan (*Message), 10)
+
+	go func() {
+		for {
+			var message *Message
+			if err := websocket.JSON.Receive(ws, &message); err != nil {
+				// TODO: close message channel and put err on an error channel
+				return
+			}
+
+			if messageMentionsUser(message, botId) {
+				messageChan <- message
+			}
+		}
+	}()
+
+	return messageChan, nil
+}
+
+func messageMentionsUser(message *Message, botId string) bool {
+	return message.Type == "message" && strings.HasPrefix(message.Text, "<@"+botId+">")
 }
 
 func (c *client) PostMessage(channel, message string) error {
--- slack/client_test.go
+++ slack/client_test.go
@@ -3,13 +3,79 @@ package slack_test
 import (
 	"github.com/mdelillo/claimer/slack"
 
+	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"golang.org/x/net/websocket"
 	"net/http"
 	"net/http/httptest"
 )
 
 var _ = Describe("Client", func() {
+	Describe("Listen", func() {
+		It("returns a channel of incoming messages mentioning the user", func() {
+			apiToken := "some-api-token"
+			botId := "some-bot-id"
+			firstMessageText := fmt.Sprintf("<@%s> some-message-text", botId)
+			secondMessageText := fmt.Sprintf("<@%s> some-other-message-text", botId)
+			messageChannel := "some-message-channel"
+
+			websocketServer := httptest.NewServer(websocket.Handler(func(ws *websocket.Conn) {
+				ws.Write([]byte(fmt.Sprintf(
+					`{"type":"%s", "text":"%s", "channel":"%s"}`,
+					"message",
+					firstMessageText,
+					messageChannel,
+				)))
+				ws.Write([]byte(fmt.Sprintf(
+					`{"type":"%s", "text":"%s", "channel":"%s"}`,
+					"not-a-message",
+					"some-non-message-text",
+					messageChannel,
+				)))
+				ws.Write([]byte(fmt.Sprintf(
+					`{"type":"%s", "text":"%s", "channel":"%s"}`,
+					"message",
+					"<@some-other-id> some-text-for-other-user",
+					messageChannel,
+				)))
+				ws.Write([]byte(fmt.Sprintf(
+					`{"type":"%s", "text":"%s", "channel":"%s"}`,
+					"message",
+					secondMessageText,
+					messageChannel,
+				)))
+			}))
+			defer websocketServer.Close()
+
+			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				defer GinkgoRecover()
+
+				Expect(r.RequestURI).To(Equal("/api/rtm.start?token=" + apiToken))
+				Expect(r.Method).To(Equal("GET"))
+
+				w.Write([]byte(fmt.Sprintf(
+					`{"ok": true, "url": "%s", "self": {"id": "%s"}}`,
+					"ws://"+websocketServer.Listener.Addr().String(),
+					botId,
+				)))
+			}))
+			defer server.Close()
+
+			incomingMessages, err := slack.NewClient(server.URL, apiToken).Listen()
+			Expect(err).NotTo(HaveOccurred())
+
+			var message *slack.Message
+			Eventually(incomingMessages).Should(Receive(&message))
+			Expect(message.Text).To(Equal(firstMessageText))
+			Expect(message.Channel).To(Equal(messageChannel))
+
+			Eventually(incomingMessages).Should(Receive(&message))
+			Expect(message.Text).To(Equal(secondMessageText))
+			Expect(message.Channel).To(Equal(messageChannel))
+		})
+	})
+
 	Describe("PostMessage", func() {
 		It("posts a message to a slack channel", func() {
 			apiToken := "some-api-token"
