6b1f1f643218cf3910f4ab4f4f68674e55a30f20
Mark DeLillo <github.med@delillo.org>
2017-03-25 08:52:51 -0400

    Add create and destroy to the help message

--- bot/commands/help_command.go
+++ bot/commands/help_command.go
@@ -6,6 +6,8 @@ func (*helpCommand) Execute() (string, error) {
 	return "Available commands:\n" +
 			"```\n" +
 			"  claim <env>     Claim an unclaimed environment\n" +
+			"  create <env>    Create a new environment\n" +
+			"  destroy <env>   Destroy an environment\n" +
 			"  owner <env>     Show the user who claimed the environment\n" +
 			"  release <env>   Release a claimed environment\n" +
 			"  status          Show claimed and unclaimed environments\n" +
